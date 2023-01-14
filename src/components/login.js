f871aafd3d00e6d21d724b00e0f30c73d29b679d
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-14 09:09:08 -0600

    set up page skeleton, deleted unnecessary files

---
+++ src/components/login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Posts = function () {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  async function submitHandler(event) {
+    event.preventDefault();
+    const response = await axios.post(
+      "https://blog-api-production-c97a.up.railway.app/login",
+      {
+        email,
+        password,
+      }
+    );
+    console.log(response);
+  }
+  return (
+    <form action="" method="POST">
+      <div className="inputGroup">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div className="inputGroup">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+      </div>
+      <button onClick={(event) => submitHandler(event)}>Submit</button>
+    </form>
+  );
+};
+
+export default Posts;
---
+++ src/components/posts.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import format from "date-fns/format";
+
+const Posts = function () {
+  let [posts, setPosts] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const postsData = await fetch(
+        "https://blog-api-production-c97a.up.railway.app"
+      );
+      const posts = await postsData.json();
+      setPosts(posts);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      {posts ? (
+        posts.map((post) => {
+          const formattedDate = format(
+            new Date(post.createdAt),
+            "MMMM Qo, yyyy"
+          );
+          return (
+            <div className="post" key={post.id}>
+              <div className="post-header">
+                <h2>{post.title}</h2>
+                <p>posted on {formattedDate}</p>
+              </div>
+              <p className="post-text">{post.text}</p>
+              {post.published ? (
+                <p className="published">Published</p>
+              ) : (
+                <p className="unpublished">Unpublished</p>
+              )}
+            </div>
+          );
+        })
+      ) : (
+        <div>
+          <p>There are no posts to display.</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Posts;
