054e5c47f1e9768bbbd0f81970ef51999f105a15
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-15 18:18:55 -0600

    created modal context, function to close modal and function to create modal

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,8 @@ import { Outlet } from "react-router";
 import ScreenBlock from "./ScreenBlock";
 import Modal from "./Modal";
 
+export const ModalContext = React.createContext();
+
 const Layout = function () {
   const [blockScreen, setBlockScreen] = useState(false);
   const [showModal, setShowModal] = useState(false);
@@ -11,6 +13,40 @@ const Layout = function () {
   const [modalText, setModalText] = useState(null);
   const [modalButtonFunc, setModalButtonFunc] = useState(null);
   const [modalButtonText, setModalButtonText] = useState(null);
+
+  const modalCloseFunc = async function (reset = true) {
+    if (reset) {
+      setModalText(null);
+      setModalTitle(null);
+      setModalButtonFunc(null);
+      setModalButtonText(null);
+    }
+    await setShowModal(false);
+    setBlockScreen(false);
+  };
+
+  //function given to ModalContext that lets components display custom modals
+  function createModal(
+    title,
+    text,
+    buttonFunc,
+    buttonText,
+    blockScreen = true
+  ) {
+    Promise.all([
+      setModalTitle(title),
+      setModalText(text),
+      setModalButtonFunc(() => buttonFunc),
+      setModalButtonText(buttonText),
+    ])
+      .then(async () => {
+        if (blockScreen) {
+          await setBlockScreen(true);
+          setShowModal(true);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 
   return (
     <div>
