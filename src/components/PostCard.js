935d89348a75f199df08b6933f070420cb68488e
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-15 18:19:47 -0600

    created PostCard component (used to be inside PostsPage)

---
+++ src/components/PostCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import format from "date-fns/format";
+
+const PostCard = function (props) {
+  return (
+    <Link to={`/post/${props.post.id}`}>
+      <div className="post" key={props.post.id}>
+        <div className="post-header">
+          <div className="flexRow">
+            <h2>{props.post.title}</h2>
+          </div>
+          <p>
+            posted on {format(new Date(props.post.createdAt), "MMMM Qo, yyyy")}
+          </p>
+        </div>
+        <p className="post-text">{props.post.text}</p>
+        {props.post.published ? (
+          <p className="published">Published</p>
+        ) : (
+          <p className="unpublished">Unpublished</p>
+        )}
+      </div>
+    </Link>
+  );
+};
+
+export default PostCard;
