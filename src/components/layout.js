4272493d788b8acef6c60d6b11f979a510f067a7
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-15 18:18:05 -0600

    added modal and screenblock to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Outlet } from "react-router";
 
+import ScreenBlock from "./ScreenBlock";
+import Modal from "./Modal";
+
 const Layout = function () {
   return (
     <div>
-      <Outlet />
+      <ScreenBlock show={blockScreen} />
+      <Modal
+        show={showModal}
+        title={modalTitle}
+        text={modalText}
+        closeFunc={modalCloseFunc}
+        buttonFunc={modalButtonFunc}
+        buttonText={modalButtonText}
+      />
+      <div style={{ zIndex: 0, position: "relative" }}>
+        <ModalContext.Provider value={createModal}>
+          <Outlet />
+        </ModalContext.Provider>
+      </div>
     </div>
   );
 };
