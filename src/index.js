4b436304d457b00f9813c9cdfb421633455c5ad0
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-19 21:22:02 -0600

    updated basename to start with a /

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={`/${process.env.PUBLIC_URL}`}>
       <App />
     </Router>
   </React.StrictMode>
